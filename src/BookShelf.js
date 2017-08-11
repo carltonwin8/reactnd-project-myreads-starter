42adae4a84384c394e3370175337b6f8a8ce6cb6
Carlton Joseph <carlton.joseph@gmail.com>
2017-08-10 19:53:01 -0700

    fix remote book fetch code bugs

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -9,7 +9,7 @@ class BookShelf extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
           {this.props.books.map(book =>
-            <li key={Math.random()}>
+            <li key={book.id}>
               <Book book={book}/>
             </li>
           )}
