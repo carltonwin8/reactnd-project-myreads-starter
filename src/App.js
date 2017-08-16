3bbf424654f4c446d7386abbd57304f1e78cd770
Carlton Joseph <carlton.joseph@gmail.com>
2017-08-15 18:09:48 -0700

    Moved book state one component up for search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -12,12 +13,73 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    currentlyReading: [],
+    wantToRead: [],
+    read: [],
+    none: []
   }
+  componentDidMount = () => {
+    BooksAPI.update({id:"bsId"},"none").then(data => {
+      for (let [shelf, ids] of Object.entries(data)) {
+        ids.map(id =>
+          BooksAPI.get(id).then(data=> {
+            this.setState((state) => {
+              let book = {
+                id,
+                title: data.title,
+                authors: data.authors.join(" & "),
+                url: `url("${data.imageLinks.thumbnail}'")`
+              }
+              return new(function () { this[shelf] = state[shelf].concat(book) })()
+            })
+          })
+        )
+      }
+    })
+  }
+
+  _updateState = (bookId, toShelf, fromShelf) => {
+    this.setState((state) => {
+      let fromSdata = state[fromShelf]
+      let toSdata = state[toShelf]
+      let fromSnew = []
+      let toSnew = []
+      for (let l=0; l < fromSdata.length; l++) {
+        let book = fromSdata[l]
+        if (book.id === bookId) {
+          if (toShelf !== 'none') toSnew = toSdata.concat(book)
+          fromSnew = fromSdata.filter(b => b.id !== bookId)
+          break
+        }
+      }
+      let stateNew = {}
+      stateNew[fromShelf] = fromSnew
+      stateNew[toShelf] = toSnew
+      return stateNew
+    })
+  }
+
+  shelfSelect = (bookId, toShelf, fromShelf) => {
+    this._updateState(bookId, toShelf, fromShelf)
+    BooksAPI.update({id:bookId},toShelf)
+      .then(r => console.log(r)) // comment out line when not debugging
+      .catch(e => {
+        console.log(`Error! Moving book ID {bookId} from {fromShelf} to {toShelf}.`)
+        console.log(e)
+        this._updateState(bookId, fromShelf, toShelf)
+      })
+  }
+
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => ( <ListBooks/> )}/>
+        <Route exact path="/" render={() => (
+          <ListBooks
+            books={this.state}
+            shelfSelect={this.shelfSelect}
+            />
+        )}/>
         <Route path="/search" render={({history}) => ( <SearchBooks /> )}/>
       </div>
     )
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,72 +1,10 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf'
-//import PropTypes from 'prop-types';
-//import escapeRegExp from 'escape-string-regexp';
-//import sortBy from 'sort-by';
 
 class ListBooks extends Component {
-  state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: [],
-    none: []
-  }
-  componentDidMount = () => {
-    BooksAPI.update({id:"bsId"},"none").then(data => {
-      for (let [shelf, ids] of Object.entries(data)) {
-        ids.map(id =>
-          BooksAPI.get(id).then(data=> {
-            this.setState((state) => {
-              let book = {
-                id,
-                title: data.title,
-                authors: data.authors.join(" & "),
-                url: `url("${data.imageLinks.thumbnail}'")`
-              }
-              return new(function () { this[shelf] = state[shelf].concat(book) })()
-            })
-          })
-        )
-      }
-    })
-  }
-
-  _updateState = (bookId, toShelf, fromShelf) => {
-    this.setState((state) => {
-      let fromSdata = state[fromShelf]
-      let toSdata = state[toShelf]
-      let fromSnew = []
-      let toSnew = []
-      for (let l=0; l < fromSdata.length; l++) {
-        let book = fromSdata[l]
-        if (book.id === bookId) {
-          if (toShelf !== 'none') toSnew = toSdata.concat(book)
-          fromSnew = fromSdata.filter(b => b.id !== bookId)
-          break
-        }
-      }
-      let stateNew = {}
-      stateNew[fromShelf] = fromSnew
-      stateNew[toShelf] = toSnew
-      return stateNew
-    })
-  }
-
-  shelfSelect = (bookId, toShelf, fromShelf) => {
-    this._updateState(bookId, toShelf, fromShelf)
-    BooksAPI.update({id:bookId},toShelf)
-      .then(r => console.log(r)) // comment out line when not debugging
-      .catch(e => {
-        console.log(`Error! Moving book ID {bookId} from {fromShelf} to {toShelf}.`)
-        console.log(e)
-        this._updateState(bookId, fromShelf, toShelf)
-      })
-  }
-
   render() {
-    const {currentlyReading, read, wantToRead} = this.state
+    const {currentlyReading, read, wantToRead} = this.props.books
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -77,19 +15,19 @@ class ListBooks extends Component {
             title="Currently Reading"
             books={currentlyReading}
             shelf="currentlyReading"
-            shelfSelect={this.shelfSelect}
+            shelfSelect={this.props.shelfSelect}
           />
           <BookShelf
             title="Want to Read"
             books={wantToRead}
             shelf="wantToRead"
-            shelfSelect={this.shelfSelect}
+            shelfSelect={this.props.shelfSelect}
           />
           <BookShelf
             title="Read"
             books={read}
             shelf="read"
-            shelfSelect={this.shelfSelect}
+            shelfSelect={this.props.shelfSelect}
           />
           </div>
         <div className="open-search">
